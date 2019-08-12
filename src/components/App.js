eca8821da621571ddcef4a0f80f15d356668aff3
Ario Tamaddoni <ariozone6@gmail.com>
2019-08-12 16:51:00 -0700

    Modified the project folder structure for a redux app.

---
+++ src/components/App.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+function App() {
+  return (
+    <div className='ui container'>
+      <h1>Redux Blogs</h1>
+    </div>
+  )
+}
+
+export default App
---
+++ src/reducers/index.js
@@ -0,0 +1,4 @@
+import { combineReducers } from "redux"
+export default combineReducers({
+  tempReducer: () => "this is temporary to avoid the warning message."
+})
