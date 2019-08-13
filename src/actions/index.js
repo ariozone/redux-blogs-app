017c69b33189ccac9d0d563ca6f4236ac32d417e
Ario Tamaddoni <ariozone6@gmail.com>
2019-08-12 17:39:39 -0700

    Enhanced the action creator to sent http get request to the posts endpoint.

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,5 +1,8 @@
-export const fetchPosts = () => {
+import jsonPlaceholder from "../apis/jsonPlaceholder"
+export const fetchPosts = async () => {
+  const posts = await jsonPlaceholder.get("/posts")
   return {
-    type: "FETCH_POSTS"
+    type: "FETCH_POSTS",
+    payload: posts
   }
 }
